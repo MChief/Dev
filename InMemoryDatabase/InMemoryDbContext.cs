06a010f19b16ceb3bcc4376c2edaf1a1bfa87414
MChief <sebastian.neurohr@gmail.com>
2024-10-10 16:09:56 +0200

    Added API and inmemorydatabase with controller

---
+++ InMemoryDatabase/InMemoryDbContext.cs
@@ -0,0 +1,9 @@
+﻿using InMemoryDatabase.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InMemoryDatabase;
+
+public class InMemoryDbContext(DbContextOptions<InMemoryDbContext> options) : DbContext(options)
+{
+    public DbSet<Employee> Employees { get; set; }
+}
